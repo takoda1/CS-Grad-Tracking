7aef08961e4b2d998630620bd09bce4ff8ba8a0f
viggy96 <vigneshb.hp@gmail.com>
2017-09-14 21:42:18 -0400

    Initial commit

---
+++ grad_project/api/api/app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var compress = require('compress')
+
+var app = express()
+
+// express apI setup
+app
+  .use(compress())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .set('view cache', true)
+  .use(function (req, res, next) {
+    if (req.headers.uid) {
+      res.cookie('onyen', req.headers.uid, { httponly: false })
+    }
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
+  })
+
+// routes to use for api
+app
+  .use('/', express.static(path.join(__dirname, '../grad_project')))
+  .use('/api', require('./api/v0.1/index.js'))
+  .use('/api/v0.1', require('./api/v0.1/index.js'))
+
+// catch 404 and forward to error handler
+app.use(function (req, res) {
+  var err = new Error('Not Fount')
+  err.status = 404
+})
+
+// production error handler
+app.use(function (err, req, res) {
+  console.log(err)
+  res.status(err.status || 500)
+  res.json({ 'error': err.message })
+})
