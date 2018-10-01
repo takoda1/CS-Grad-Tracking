3dad069844c4e2ffdaaa97420b4e249e22f8ea9e
Svflann <svflann@ad.unc.edu>
2018-10-01 13:30:07 -0400

    Changed upload students view to add Notes image and fixed upload

--- models/schema.js
+++ models/schema.js
@@ -71,8 +71,8 @@ var studentSchema = mongoose.Schema({
   jobHistory: [{type: mongoose.Schema.Types.ObjectId, ref: "Job"}],
   semesterStarted: { type: mongoose.Schema.Types.ObjectId, ref: "Semester" },
   advisor: { type: mongoose.Schema.Types.ObjectId, ref: "Faculty" },
-  grades: [{type:mongoose.Schema.Types.ObjectId, ref: "Grade"}],
-  notes: String
+  notes: String,
+  grades: [{type:mongoose.Schema.Types.ObjectId, ref: "Grade"}]
 });
 
 // Forms
