7de0557876a4a94e5565f954af30362211dc0d11
root <root@csgrad.cs.unc.edu>
2019-02-25 10:41:25 -0500

    Lots of forms stuff.

--- routes/course.js
+++ routes/course.js
@@ -10,6 +10,17 @@ router.use(function(req, res, next){
 	});
 });
 
+router.use(function(req, res, next){
+	util.checkAdmin().then(function(result){
+		if(result){
+			next();	
+		}
+		else{
+			res.render("../views/error.ejs", {string:"Not admin"});
+		}
+	});
+});
+
 router.get("/", course.get);
 
 router.get("/edit/:_id", course.edit);
